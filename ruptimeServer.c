b5672888c0e4b5f0de98f72ae054b78abf75db3b
Autrin <autrinhakimi@gmail.com>
2024-09-23 17:15:42 -0500

    created server socket

---
+++ ruptimeServer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <bits/socket.h>
+#include <netinet/in.h>
+
+int main (int argc, char *argv[]){
+    int serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+  
+    return 0;
+}
