b05531d329aa5ec1bc766b6771d19935eac7dbad
Autrin <autrinhakimi@gmail.com>
2024-09-23 17:53:25 -0500

    fixed the clinent address size for accept

--- ruptimeServer.c
+++ ruptimeServer.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-#include <bits/socket.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 
 int main (int argc, char *argv[]){
     int serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serverAddr, clientAddr;
+
     if (serverSocket < 0){
         perror("Error creating socket\n");
         return 1;
     }
     printf("Socket created\n");
 
-    struct sockaddr_in serverAddr, clientAddr;
-    serverAddr.sin_family = PF_INET;
+    serverAddr.sin_family = PF_INET; // communicatio domain
     serverAddr.sin_port = htons(atoi(argv[1])); // server port to send a message to
     serverAddr.sin_addr.s_addr = inet_addr(INADDR_ANY); // Server ip address to send a message to
     
@@ -26,10 +27,13 @@ int main (int argc, char *argv[]){
         return 1;
     }
     printf("Listening on socket\n");
-
-    int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddr, sizeof(clientAddr)); //
+    socklen_t clientAddrSize = sizeof(clientAddr);
+    int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddr, &clientAddrSize); // server accepts an incoming connection
     if(clientSocket < 0){
         perror("Error accepting connection\n");
     }
+    printf("Connection accepted\n");
+
+
     return 0;
 }
