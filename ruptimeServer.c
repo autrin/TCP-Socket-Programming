dc4fd145e8ba422608d40d2c6f403f39ff405923
Autrin <autrinhakimi@gmail.com>
2024-09-23 17:17:29 -0500

    error checking for serversocket

--- ruptimeServer.c
+++ ruptimeServer.c
@@ -4,6 +4,11 @@
 
 int main (int argc, char *argv[]){
     int serverSocket = socket(PF_INET, SOCK_STREAM, 0);
-  
+    if (serverSocket < 0){
+        printf("Error creating socket\n");
+        return 1;
+    }
+    printf("Socket created\n");
+
     return 0;
 }
