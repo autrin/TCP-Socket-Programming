b618cdd3af9d678f89ef8cdbdc3db6ee38ffdc5d
Autrin <autrinhakimi@gmail.com>
2024-09-23 18:03:40 -0500

    setting up the server addres

--- ruptimeClient.c
+++ ruptimeClient.c
@@ -6,6 +6,10 @@ int main(int argc, char *argv[]){
     int clientSocket;
     struct sockaddr_in serverAddr;
     
+    serverAddr.sin_family = PF_INET; // communicatio domain
+    serverAddr.sin_port = htons(atoi(argv[1])); // server port to send a message to
+    serverAddr.sin_addr.s_addr = inet_addr(INADDR_ANY); // Server ip address to send a message to
+    
     if(clientSocket = socket(PF_INET, SOCK_STREAM, 0) < 0){
         perror("Error creating client socket\n");
         return 1;
