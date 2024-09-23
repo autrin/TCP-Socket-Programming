593c8673d901f15499efb7bc66ce8c5dc88b6cff
Autrin <autrinhakimi@gmail.com>
2024-09-23 17:57:45 -0500

    error checking of socker creation

--- ruptimeClient.c
+++ ruptimeClient.c
@@ -2,6 +2,11 @@
 #include <sys/socket.h>
 
 int main(int argc, char *argv[]){
-    int clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+    int clientSocket;
+    if(clientSocket = socket(PF_INET, SOCK_STREAM, 0) < 0){
+        perror("Error creating client socket\n");
+        return 1;
+    }
+    
     return 0;
 }
