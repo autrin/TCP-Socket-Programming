e54756630d27670143c583c03309047284848518
Autrin <autrinhakimi@gmail.com>
2024-09-23 18:03:01 -0500

    created server address

--- ruptimeClient.c
+++ ruptimeClient.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(int argc, char *argv[]){
     int clientSocket;
+    struct sockaddr_in serverAddr;
+    
     if(clientSocket = socket(PF_INET, SOCK_STREAM, 0) < 0){
         perror("Error creating client socket\n");
         return 1;
     }
+    printf("Client socket created\n");
+
     
     return 0;
 }
